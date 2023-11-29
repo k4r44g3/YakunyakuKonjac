e515ddbeb9f9db23cf60dc0d262fdc969e14f18c
okada <oky2284901@stu.o-hara.ac.jp>
2023-11-29 12:24:23 +0900

    テスト用アイコン画像の追加

--- tools/installer/test.bat
+++ tools/installer/test.bat
@@ -9,8 +9,68 @@ call Scripts\activate.bat
 @REM パッケージインストール タイムアウトを100秒に変更
 
 @REM AWS関連
-pip --default-timeout=100 install pywin32
+pip --default-timeout=100 install awscli
+pip --default-timeout=100 install boto3
+@REM OCR関連
+pip --default-timeout=100 install easyocr
+@REM 翻訳関連
+pip --default-timeout=100 install deep-translator
+@REM GUI関連
+pip --default-timeout=100 install keyboard
+pip --default-timeout=100 install pyautogui
+pip --default-timeout=100 install PySimpleGUI
 
 @REM パッケージ一覧出力ファイルの作成
 pip freeze > requirements.txt
-pause
+
+@REM gitからクローン
+git clone https://github.com/pppp-987/YakunyakuKonjac.git
+
+@REM プロジェクトファイルに移動
+cd YakunyakuKonjac
+
+@REM ブランチ変更
+git checkout environment
+
+@REM ショートカット作成 VBScript使用
+
+@REM @REM ショートカットのリンク先パスの設定
+set ShortcutPath="%~dp0\app.lnk"
+
+@REM @REM ショートカットを作成したいファイルのパス
+set TargetPath="%cd%\tools\app.bat"
+
+@REM @REM ショートカットのアイコンのパス
+set ShortcutIconPath="%cd%\static\icon\app.ico"
+
+@REM 作業フォルダのパス
+set WorkingDirectoryPath="%cd%\tools"
+
+@REM 一時的なVBScriptファイルの作成
+copy nul CreateShortcut.vbs
+
+@REM WScript.Shellオブジェクトの作成
+echo Set WScriptShell = WScript.CreateObject("WScript.Shell") >> CreateShortcut.vbs
+
+@REM ショートカットの作成
+echo Set Shortcut = WScriptShell.CreateShortcut(%ShortcutPath%) >> CreateShortcut.vbs
+
+@REM ショートカットのリンク先パスの設定
+echo Shortcut.TargetPath = %TargetPath% >> CreateShortcut.vbs
+
+@REM 作業フォルダの設定
+echo Shortcut.WorkingDirectory = %WorkingDirectoryPath% >> CreateShortcut.vbs
+
+@REM ショートカットアイコンの設定
+echo Shortcut.IconLocation = %ShortcutIconPath% >> CreateShortcut.vbs
+
+@REM ショートカットを保存
+echo Shortcut.Save >> CreateShortcut.vbs
+
+@REM VBSファイルを実行してショートカットを作成
+cscript CreateShortcut.vbs
+
+@REM 一時的に作成したVBSファイルを削除
+del CreateShortcut.vbs
+
+msg * インストールが完了しました
