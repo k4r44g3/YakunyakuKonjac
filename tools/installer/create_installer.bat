b053bd21b939dcf01a74356b41f46bd204309ae3
okada <oky2284901@stu.o-hara.ac.jp>
2023-11-30 12:47:41 +0900

    exeファイルの更新

--- tools/installer/create_installer.bat
+++ tools/installer/create_installer.bat
@@ -6,6 +6,22 @@
 @REM 文字コードを"Shift-JIS"に設定 メッセージは非表示
 chcp 932 >nul
 
+@REM 必要なファイルが存在しない場合、終了する
+if not exist "installer_public.py" (
+    msg * installer_public.pyが存在しません
+    exit
+)
+
+if not exist "installer_private.py" (
+    msg * installer_private.pyが存在しません
+    exit
+)
+
+if not exist "app.ico" (
+    msg * app.icoが存在しません
+    exit
+)
+
 @REM 仮想環境作成
 py -3.8 -m venv venv_installer
 @REM カレントディレクトリを仮想環境のルートディレクトリにする
@@ -28,19 +44,17 @@ md src
 
 @REM インストーラのスクリプトのコピー
 copy ..\installer_public.py src
-
 copy ..\installer_private.py src
 
+@REM アイコンのコピー
+copy ..\app.ico src
+
 @REM exeファイルの作成(1つのファイルにまとめる、コンソール非表示、キャッシュの削除, 名前指定)
-pyinstaller src\installer_public.py --onefile --noconsole --clean --name installer_public.exe
+pyinstaller src\installer_public.py --onefile --noconsole --clean --name=installer_public.exe --icon=src\app.ico
+pyinstaller src\installer_private.py --onefile --noconsole --clean --name=installer_private.exe --icon=src\app.ico
 
 @REM exeファイルのコピー
 copy dist\installer_public.exe ..\
-
-@REM exeファイルの作成(1つのファイルにまとめる、コンソール非表示、キャッシュの削除, 名前指定)
-pyinstaller src\installer_private.py --onefile --noconsole --clean --name installer_private.exe
-
-@REM exeファイルのコピー
 copy dist\installer_private.exe ..\
 
 @REM 仮想環境から抜け出す
